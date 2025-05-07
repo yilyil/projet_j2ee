a82c39d940cd85ca9a869641feea5e4f8ce3894c
abdelbaki bouzaienne <Abdelbaki.bouzaienne@yahoo.fr>
2025-05-07 17:56:25 +0200

    Update TestGreeter.java
    .

--- server/src/test/java/com/example/TestGreeter.java
+++ server/src/test/java/com/example/TestGreeter.java
@@ -1,33 +1,21 @@
 package com.example;
 
-import org.junit.Before;
-import org.junit.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.Matchers.greaterThan;
-import static org.junit.Assert.assertThat;
-import static org.junit.matchers.JUnitMatchers.containsString;
+import org.junit.jupiter.api.Test;
 
 public class TestGreeter {
-
-  private Greeter greeter;
-
-  @Before
-  public void setup() {
-    greeter = new Greeter();
-  }
-
-  @Test
-  public void greetShouldIncludeTheOneBeingGreeted() {
-    String someone = "World";
-
-    assertThat(greeter.greet(someone), containsString(someone));
-  }
-
-  @Test
-  public void greetShouldIncludeGreetingPhrase() {
-    String someone = "World";
-
-    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
-  }
+    
+    @Test
+    public void testGreetContains() {
+        String result = new Greeter().greet("World");
+        assertThat(result, containsString("Hello"));
+    }
+    
+    @Test
+    public void testGreetLength() {
+        String result = new Greeter().greet("World");
+        assertThat(result.length(), greaterThan(5));
+    }
 }
